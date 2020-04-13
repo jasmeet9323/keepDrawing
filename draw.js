efdc823fd33eaa062ed4d0e4d84187db73229606
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2020-04-12 18:50:17 -0700

    Add border to the canvas

--- draw.js
+++ draw.js
@@ -2,19 +2,21 @@ $(document).ready(function () {
     var canvas = $("#myCanvas");
     var ctx = canvas.get(0).getContext("2d");
 
-    ctx.strokeStyle="#808080";
+    ctx.strokeStyle="rgba(0,0,0,0.2)";
 
     var width = canvas.width();
     var height = canvas.height();
 
+    // dimension of the drawing
+    var dimension = 25;
     var i;
-    for(i = 0; i <= width; i+=width/10) {
+    for(i = 0; i <= width; i+=width/dimension) {
         ctx.beginPath();
         ctx.moveTo(i,0);
         ctx.lineTo(i,height);
         ctx.stroke();
     }
-    for(i=0; i <= height; i+=height/10) {
+    for(i=0; i <= height; i+=height/dimension) {
         ctx.beginPath();
         ctx.moveTo(0,i);
         ctx.lineTo(width,i);
--- draw.php
+++ draw.php
@@ -5,7 +5,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="draw.js"></script>
 
-    <canvas id="myCanvas" width="500" height="500">
+    <canvas id="myCanvas" width="500" height="500" style="border: 1px #000 solid">
         Your browser does not support the HTML5 canvas tag.
     </canvas>
 </body>
