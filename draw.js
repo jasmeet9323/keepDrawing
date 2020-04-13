2692083d4a5ba5b10df6341cb8a3060382208880
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2020-04-12 18:44:02 -0700

    Show the canvas as a grid.

--- draw.js
+++ draw.js
@@ -2,17 +2,23 @@ $(document).ready(function () {
     var canvas = $("#myCanvas");
     var ctx = canvas.get(0).getContext("2d");
 
-    ctx.strokeStyle="#ff0000";
+    ctx.strokeStyle="#808080";
 
     var width = canvas.width();
     var height = canvas.height();
 
     var i;
-    for(i = 0; i < width; i+=10) {
+    for(i = 0; i <= width; i+=width/10) {
         ctx.beginPath();
         ctx.moveTo(i,0);
-        ctx.lineTo(i,100);
+        ctx.lineTo(i,height);
         ctx.stroke();
-    }    
+    }
+    for(i=0; i <= height; i+=height/10) {
+        ctx.beginPath();
+        ctx.moveTo(0,i);
+        ctx.lineTo(width,i);
+        ctx.stroke();
+    }
 
 });
--- draw.php
+++ draw.php
@@ -5,7 +5,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="draw.js"></script>
 
-    <canvas id="myCanvas" width="100" height="100">
+    <canvas id="myCanvas" width="500" height="500">
         Your browser does not support the HTML5 canvas tag.
     </canvas>
 </body>
