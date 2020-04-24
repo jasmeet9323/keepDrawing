3b92551c519743fba008448b67547e15282472d5
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2020-04-24 15:28:08 -0700

    Highlight the miniCanvas that is hovered on.

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
 <html>
+<style>
+    body {
+        background-color: #f0f0f0;
+        margin: 20px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    #bigCanvas {
+        background-color: #fff;
+        cursor: pointer;
+    }
 
+    #bigCanvasWrapper {
+        position: relative;
+    }
+
+    #selectionBox {
+        border: 1px rgba(0, 50, 100, 0.5);
+        position: absolute;
+        pointer-events: none;
+        background-color: gray;
+    }
+
+</style>
 <body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="static/index.js"></script>
-    <canvas id="bigCanvas" width="800" height="800" style="border: 1px #000 solid"></canvas>    
+    <div id="bigCanvasWrapper">
+        <canvas id="bigCanvas" width="800" height="800" style="border: 1px #000 solid"></canvas>    
+    </div>
 </body>
 
 </html>
--- static/index.js
+++ static/index.js
@@ -6,22 +6,52 @@ $(document).ready(function(){
     let dimension = 25;
     let repeatX = 20;
     let repeatY = 20;
+    let selectionBox = null;
     
     let width = dimension * repeatX * pixelSize;
     let height = dimension * repeatY * pixelSize;
 
-    for(let i = 0; i < dimension * repeatX * pixelSize; i+= dimension) {
+    ctx.strokeStyle="rgba(0,0,0,0.2)";
+
+    // Draw grid
+    for(let i = 0; i < dimension * repeatX * pixelSize; i+= dimension*pixelSize) {
         ctx.beginPath();
         ctx.moveTo(i,0);
         ctx.lineTo(i,height);
         ctx.stroke();
     }
 
-    for(let j = 0; j < dimension * repeatY * pixelSize; j+= dimension) {
+    for(let j = 0; j < dimension * repeatY * pixelSize; j+= dimension * pixelSize) {
         ctx.beginPath();
         ctx.moveTo(0,j);
         ctx.lineTo(width,j);
         ctx.stroke();
     }
 
+    // Canvas response
+    canvas.click(function(e) {
+        selectBox(e);
+    });
+
+    canvas.mousemove(function(e) {
+        let pixel = [Math.floor(e.offsetX / (pixelSize * dimension)), Math.floor(e.offsetY / (pixelSize * dimension))];
+        console.log(pixel);
+        if(!selectionBox) {
+            selectionBox = $("<div id=selectionBox></div>");
+            selectionBox.css({width: dimension*pixelSize, height:dimension * pixelSize});
+            $("#bigCanvasWrapper").prepend(selectionBox);
+        }
+        selectionBox.css({left: pixel[0] * pixelSize * dimension+1,
+            top: pixel[1] * pixelSize * dimension+1});
+    });
+
+    let isSelected = 0;
+
+    function selectBox(e) {
+        if(isSelected) return;
+        isSelected = 1;
+
+        let pixel = [Math.floor(e.offsetX / (pixelSize * dimension)), 
+        Math.floor(e.offsetY / (pixelSize * dimension))];
+    }
 });
