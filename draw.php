6e91e99213b87dea222e4dde9f724f27067c526a
jasmeet9323 <jasmeet.singh.mec11@iitbhu.ac.in>
2020-04-30 17:43:09 -0700

    Include a reset button on drawing page

--- draw.php
+++ draw.php
@@ -1,8 +1,11 @@
 <?php
 
+// initialize variables
 $x = intval($_REQUEST['x']);
 $y = intval($_REQUEST['y']);
 $_REQUEST['submit'] = isset($_REQUEST['submit']) ? $_REQUEST['submit'] : '';
+
+// save the data using python script and command line
 if ($_REQUEST['submit']) {
     $data = $_POST['data'];
     $data = json_encode($data);
@@ -34,13 +37,23 @@ print <<< EOF
     <script src="static/draw.js"></script>
     <script src="static/pickr/dist/pickr.min.js"></script>
 
+    <!-- Back Button -->
+    <div>
+        <button type="button" value="go back" onclick="location.href='index.php'" style="margin-bottom: 1em;">
+            Back
+        </button>
+    </div>
+
     <!-- Color Picker-->
     <div style="display: flex; margin-bottom: 1em;">
         <div id="pickr"></div>
         <div style="padding-left: 1em;">
-            <button type="button" value="choose color" onclick="PICKR.show()" style="display: inline;">Choose Color</button>
+            <button type="button" value="choose color" onclick="PICKR.show()">Choose Color</button>
         </div>
-    </div>
+        <div id="reset" style="padding-left: 3em">
+            <button type="button" value="reset">Reset</button>
+        </div>
+    </div>  
 
     <!-- Small canvas -->
     <div id="canvas" style="margin-bottom: 1em">
